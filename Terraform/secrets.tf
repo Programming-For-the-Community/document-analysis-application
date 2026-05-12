e93ac9289308cd9fefc0f7f09a9bf6de72941e0e
ProfessorChaos0802 <1822rhit@pm.me>
2026-05-11 21:40:59 -0500

    Fixing tf format

--- Terraform/secrets.tf
+++ Terraform/secrets.tf
@@ -10,16 +10,16 @@ resource "aws_secretsmanager_secret_version" "app_config" {
     COGNITO_USER_POOL_ID          = aws_cognito_user_pool.users.id
     COGNITO_CLIENT_ID             = aws_cognito_user_pool_client.electron_app.id
     S3_BUCKET                     = aws_s3_bucket.documents.bucket
-    DYNAMODB_PROJECT_STATE_TABLE   = aws_dynamodb_table.project_state.name
-    DYNAMODB_PROJECT_ACCESS_TABLE  = aws_dynamodb_table.project_access.name
-    DYNAMODB_USER_SESSIONS_TABLE   = aws_dynamodb_table.user_sessions.name
+    DYNAMODB_PROJECT_STATE_TABLE  = aws_dynamodb_table.project_state.name
+    DYNAMODB_PROJECT_ACCESS_TABLE = aws_dynamodb_table.project_access.name
+    DYNAMODB_USER_SESSIONS_TABLE  = aws_dynamodb_table.user_sessions.name
     SQS_QUEUE_URL                 = aws_sqs_queue.textract_results.url
     SNS_TOPIC_ARN                 = aws_sns_topic.textract_results.arn
     BEDROCK_MODEL_ID              = var.bedrock_model
-    QDRANT_KEY                     = var.qdrant_api_key
-    SVC_PWD                        = var.svc_acct_password
-    SVC_USER                       = var.svc_acct_username
-    NEO4J_URI                      = var.neo4j_uri
-    QDRANT_URL                     = var.qdrant_url
+    QDRANT_KEY                    = var.qdrant_api_key
+    SVC_PWD                       = var.svc_acct_password
+    SVC_USER                      = var.svc_acct_username
+    NEO4J_URI                     = var.neo4j_uri
+    QDRANT_URL                    = var.qdrant_url
   })
 }
